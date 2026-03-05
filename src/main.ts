bd70e37a1c37a89b65943dac6639247356e9f68c
Zach Schwartz <zachatorcodes@gmail.com>
2026-03-05 14:48:40 -0700

    Add basic Swagger API info

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Outdoor Blog CRUD API')
+    .setDescription('The CRUD API for Zach\'s Outdoor Blog Backend Application.')
+    .setVersion('0.1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
