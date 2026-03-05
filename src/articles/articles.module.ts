61d3ddba7e5a10cff56687bf70471aaa290107f4
Zach Schwartz <zachatorcodes@gmail.com>
2026-03-05 15:39:04 -0700

    Import the Prisma Module into the Aricles Module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service.js';
 import { ArticlesController } from './articles.controller.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ArticlesController],
   providers: [ArticlesService],
 })
