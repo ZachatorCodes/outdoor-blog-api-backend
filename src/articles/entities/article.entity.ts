9b5687d0d4be6add71f99b398273a7aac63bcda9
Zach Schwartz <zachatorcodes@gmail.com>
2026-03-05 18:55:59 -0700

    Add nullable again

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -8,8 +8,8 @@ export class ArticleEntity implements Article {
   @ApiProperty()
   title: string;
 
-  @ApiProperty({ required: false})
-  description: string;
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
 
   @ApiProperty()
   body: string;
